21646a765bf5287ee76c0c852d2eac2cfe23473a
Marchons <d1286360646@outlook.com>
2023-03-28 21:17:50 +0800

    变更

--- src/main/java/com/scut/community/scutbbs/ScutbbsApplication.java
+++ src/main/java/com/scut/community/scutbbs/ScutbbsApplication.java
@@ -2,7 +2,7 @@ package com.scut.community.scutbbs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+// 废弃
 @SpringBootApplication
 public class ScutbbsApplication {
 
